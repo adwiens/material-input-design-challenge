f5122ea2200556c1242fc5a79e13e260852dd816
Drew Wiens <adwiens@gmail.com>
2020-09-24 23:27:12 -0500

    WIP typography

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'itn-dc';
+  @ViewChild('activeInput') set ai(ref: ElementRef<HTMLInputElement>) {
+    setInterval(() => ref.nativeElement.focus(), 1000); // Focus active input
+  }
+  errorControl = new FormControl('', Validators.required);
+
+  constructor() {
+    this.errorControl.markAsTouched(); // Always show error state
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatInputModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
