e18ba8dcbbf4fe5f0fd3835d4c60fa287a912ab0
Drew Wiens <adwiens@gmail.com>
2020-09-25 00:58:06 -0500

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  @ViewChild('activeInput') set ai(ref: ElementRef<HTMLInputElement>) {
-    setInterval(() => ref.nativeElement.focus(), 1000); // Focus active input
+export class AppComponent implements AfterViewInit {
+  @ViewChild('activeInput') set input(ref: ElementRef<HTMLInputElement>) {
+    setInterval(() => ref.nativeElement.focus(), 1000); // Focus "active" input
   }
-  errorControl = new FormControl('', Validators.required);
+  @ViewChildren(MatFormField) formFields: QueryList<MatFormField>;
+  errorControl = new FormControl('Input Text', Validators.minLength(15));
 
   constructor() {
-    this.errorControl.markAsTouched(); // Always show error state
+    this.errorControl.markAsTouched(); // Show error state
+  }
+
+  ngAfterViewInit(): void {
+    // Fix for Material bug where label whitespace (the "outline gap")
+    // remains the width of the native (non-Lato) font:
+    this.formFields.forEach(field => setTimeout(() => field.updateOutlineGap(), 500));
   }
 }
